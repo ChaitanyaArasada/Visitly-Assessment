4108637f1960978cd69558498ed8c56b5779b20d
Chaitanya Arasada <chaitanyaarasada300@gmail.com>
2025-01-15 16:02:58 +0530

    Create UserList.jsx

---
+++ UserList.jsx
@@ -0,0 +1,28 @@
+ import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const UserList = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/api/users")
+      .then((response) => setUsers(response.data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  return (
+    <div>
+      <h1>User List</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} ({user.email})
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UserList;
